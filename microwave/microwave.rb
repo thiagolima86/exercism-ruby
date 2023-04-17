1ce41fe0e5bba868824206641d827200924e851c
Thiago Lima <thiagolima86@gmail.com>
2023-04-17 00:17:47 -0300

    passed all tests

--- microwave/microwave.rb
+++ microwave/microwave.rb
@@ -1,7 +1,29 @@
-=begin
-Write your code for the 'Microwave' exercise in this file. Make the tests in
-`microwave_test.rb` pass.
+class Microwave
+  def initialize(digit)
+    @digit = digit
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/microwave` directory.
-=end
+  def timer
+    h = 0
+    m = @digit
+    if @digit > 60
+      h = @digit / 60
+      m = @digit % 60
+    end
+
+    digit_array = @digit.digits.reverse
+
+    if digit_array.length >= 3
+      m = digit_array.pop(2).join.to_i
+      h = digit_array.join.to_i
+
+      if m > 60
+        h += m / 60
+        m = m % 60
+      end
+    end
+    
+    time = Time.new(1, 1, 1, 0, h, m)
+    time.strftime("%M:%S")
+  end
+end
